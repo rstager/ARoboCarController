f979ef6a8b0793f967ef37139e7c012b1f5a41e4
rstager <roger@stager.us>
2017-07-18 18:56:50 -0700

    temp save

--- model_v2.py
+++ model_v2.py
@@ -4,6 +4,7 @@ from keras.models import Sequential, Model, Input
 from keras.optimizers import Adam
 import numpy as np
 import pickle
+from h5utils import H5Recorder
 
 
 def createModel(config):
@@ -11,7 +12,7 @@ def createModel(config):
     width = config["camerawidth"]
     # Create the model
     inp = Input((height,width, 3),name="frontcamerainput")
-    sensors = Input((3,),name="speedinput") # speed, accelleration, throttle
+    sensors = Input((3,),name="sensors") # speed, accelleration, throttle
     model = Sequential()
     model.add(Conv2D(32,(3, 3), input_shape=(height,width, 3), padding='same', activation='relu'))
     model.add(BatchNormalization())
@@ -44,24 +45,53 @@ def createModel(config):
     combined_model.compile(loss='mean_squared_error', optimizer=opt)
     return combined_model
 
-def State2X(state):
-    img = state["frontcamera"]
-    speed = np.reshape(np.array([state["speed"]*.001,state["delta_speed"]*.001,state['throttle']]),(1,3))
+# convert observation+info structure and normalize for model
+def preprocess(observation,info):
+    ret=observation
+    ret[1] *= np.array([0.001,0.001,0.00001])
+    return ret
+
+#convert output of model to structure/units used by arobocar
+def converta(actions):
+    return actions
+
+#convert observation+info to input to model
+def convertX(observation,info):
+    img = observation[0]
     img = np.reshape(img, (1, img.shape[0], img.shape[1], img.shape[2]))
-    ec=np.array([state["ec.goal_speed"],state["ec.speed_delta"],state["ec.speed_integral"]])
-    return [img,speed,ec] #if more state is extracted than used by model, it will be ignored
+    sensor = observation[1]
+    sensor = np.reshape(np.array([sensor[0] * .001, sensor[1] * .001, 0.0]), (1, 3))
+    return [img,sensor]
 
-def createDatasets(config,output,maxidx):
-    height=config["cameraheight"]
-    width = config["camerawidth"]
-    images = output.create_dataset('frontcamera', (maxidx, height, width, 3), 'i1')
-    images.attrs['description'] = "simple test"
-    speeds = output.create_dataset('speed', (maxidx, 3), 'f')
-    ec=output.create_dataset('ec', (maxidx, 3), 'f')
-    output.attrs["config"]=pickle.dumps(config,0)
-    return [images,speeds,ec]
+scaled=np.array([0.001,0.001,0.00001])
+#record any additional items
+class recorder(H5Recorder):
+    def __init__(self,h5file,config=None,maxidx=10000):
+        super().__init__(h5file, config,maxidx,preprocess=preprocess,postprocess=postprocess)
+        if config: # only set for create
+            self.ec=h5file.create_dataset('ec', (maxidx, 3), 'f')
+            self.ec.attrs['cols']="speed,acceleration,odometer"
+        else:
+            self.ec=h5file['ec']
 
-def getDatasets(input):
-    config= pickle.loads(input.attrs["config"])
-    nsamples = input['frontcamera'].shape[0]
-    return config, nsamples, [input['frontcamera'],input['speed'],input['ec']]
+    # record a single observation
+    def record(self, observation, reward, done, info):
+        idx=super().record(observation,reward,done,info)
+        self.ec[idx]=info['ec']
+        return idx
+
+    # get an array of observations
+    def get(self, idx, cnt=1):
+        observation, reward, done, info = super().get(idx,cnt)
+        for i in range(cnt):
+            info[i]['ec']=self.ec[idx+i]
+        return observation, reward, done, info
+
+
+    # returns an array of observations,reward,..
+    def getX(self,idx,cnt=1):
+        X=[self.images[idx:idx+cnt],self.sensors[idx:idx+cnt]*scaled]
+        return X
+
+
+
