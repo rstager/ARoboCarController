42cc1b53f1e93457eb30fe68f913a20e1e2455aa
rstager <roger@stager.us>
2017-07-22 20:46:37 -0700

    Changed to pass observercode in config. Requires latest ARoboCar AIAgent.py

--- arobocar.py
+++ arobocar.py
@@ -5,6 +5,9 @@ import importlib.util
 import gym
 from gym import spaces
 import numpy as np
+#controller side functions
+
+
 
 # This class an aigym connection to the RoboCar simulator
 
@@ -19,9 +22,8 @@ class ARoboCar(gym.Env):
         observations=[]
         observations.append(spaces.Box(low=0, high=255, shape=(config['cameraheight'], config['camerawidth'], 3)))
         observations.append(spaces.Box(np.array([-1.0,-1.0,0.0]), np.array([1.0,1.0,100000.0]))) # speed, accel, odometer
-        if('observer' in self.config):
-            observer = importlib.import_module(observermodule)
-            observations.append(observer.spaces(config))
+        #if('observer' in self.config):
+        #    observations.append(self.spaces(config))
         print(observations)
         self.observation_space = spaces.Tuple(observations)
 
@@ -90,7 +92,9 @@ class ARoboCar(gym.Env):
             else:
                 config.update(confighook)
         if('observer' in config):
-            config['observer']=importlib.util.find_spec(config['observer']).origin
+            with open(config['observer']+'.py', "r") as myfile:
+                config['observercode'] = myfile.read()
+#            config['observer']=importlib.util.find_spec(config['observer']).origin
         pickle.dump(config,self.fcmd)
         self.fcmd.flush()
         self.connected=True
